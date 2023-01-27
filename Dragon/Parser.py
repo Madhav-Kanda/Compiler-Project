b0ef51fea5edc1eef4dbca9b59b759b351159683
MihirSutariya <sutariya.mihirkumar@iitgn.ac.in>
2023-01-27 13:59:17 +0530

    comments added in parser.py

--- Dragon/Parser.py
+++ Dragon/Parser.py
@@ -3,6 +3,9 @@ from Expr import *
 
 
 class Parser:
+    """
+    class for making AST tree for expressions
+    """
     
     class ParseError(Exception):
         pass
@@ -10,7 +13,8 @@ class Parser:
     current =0 
     def __init__(self, tokens):
         self.tokens = tokens
-        
+    
+    # return head of the root node 
     def parse(self):
         try:
             x = self.expression()
@@ -18,9 +22,11 @@ class Parser:
         except :
             return None
                 
+    # parse the expression and return root of AST
     def expression(self):
         return self.equality()
     
+    # parse equality operator
     def equality(self):
         expr = self.comparison()
         while(self.match([TokenType.BANG_EQUAL,TokenType.EQUAL_EQUAL])):
@@ -30,6 +36,7 @@ class Parser:
             
         return expr
     
+    # if match with given types return True
     def match(self, types):
         for type in types:
             if self.check(type):
@@ -38,23 +45,29 @@ class Parser:
         
         return False
     
+    # check current token is particular type or not
     def check(self, type):
         if(self.isAtEnd()): return False
         return self.peek().type == type;
     
+    #  return current token and increase pointer by 1
     def advance(self):
         if not self.isAtEnd(): self.current+=1
         return self.previous()
     
+    # check if pointer is at end of the code file 
     def isAtEnd(self) :
         return self.peek().type == TokenType.EOF;
     
+    # returns current token
     def peek(self) :
         return self.tokens[self.current];
   
+    # return previouse token
     def previous(self):
         return self.tokens[self.current - 1];
 
+    # grammar non terminal comparison return expression with comparision operator as root if exists
     def comparison(self):
         expr = self.term()
         
@@ -65,6 +78,7 @@ class Parser:
             
         return expr
     
+    # grammar non terminal comparison return expression with (+,-) operator as root if exists
     def term(self):
         expr = self.factor()
         while self.match([TokenType.MINUS, TokenType.PLUS]):
@@ -73,6 +87,7 @@ class Parser:
             expr = Binary(expr, operator, right)
         return expr
     
+    # grammar non terminal comparison return expression with (/,*) operator as root if exists
     def factor(self):
         expr = self.unary()
         while(self.match([TokenType.SLASH,TokenType.STAR])):
@@ -81,14 +96,14 @@ class Parser:
             expr = Binary(expr,operator,right)
             
         return expr
-    
+    # grammar non terminal comparison return expression with (!,-) operator as root if exists
     def unary(self):
         if( self.match([TokenType.BANG,TokenType.MINUS])):
             operator = self.previous()
             right = self.unary()
             return Unary(operator,right)
         return self.primary()
-    
+    # return leaf node of the tree
     def primary(self):
         if self.match([TokenType.FALSE]) : return Literal(False)
         if self.match([TokenType.TRUE]) : return Literal(True)
@@ -111,7 +126,8 @@ class Parser:
     def error(self,token,message):
         dragon.Dragon.error(token,message)
         return self.ParseError()
-    
+    # synchronizing errors if we got an error don't kill the program try find other errors 
+    # to do this if we got error in 1st line skip that line and go to next line
     def synchronize(self):
         self.advance()
         while(not self.isAtEnd()):
